f2bceffdbb2a0d38e576cc1c60ee4b480ed7e1f8
DPutra17 <effandyputra45@gmail.com>
2024-12-15 15:05:11 +0700

    rancangan awal

--- UAS.cpp
+++ UAS.cpp
@@ -2,3 +2,35 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// Struktur untuk Kelas (Double Linked List)
+
+// Struktur untuk Stack Aktivitas
+
+// Struktur untuk Queue Belajar
+
+// Struktur untuk Tugas (Single Linked List)
+
+// Variabel Global
+
+
+
+// menu utama
+
+// Menu kelas 
+
+// menu Aktifitas 
+
+// menu antrian belajar 
+
+// menu tugas 
+
+
+
+// Fungsi kelas 
+
+// Fungsi Aktifitas 
+
+// Fungsi antrian belajar 
+
+// Fungsi menu tugas 
