00f94dce4430622422c9ee88156ba58f511d692c
DPutra17 <effandyputra45@gmail.com>
2024-12-15 15:19:27 +0700

    test3

--- UAS.cpp
+++ UAS.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
-    
+
 // Struktur untuk Kelas (Double Linked List)
 
 // Struktur untuk Stack Aktivitas
@@ -13,7 +13,7 @@
 
 // Variabel Global
 
-
+//testteerr
 
 // menu utama
 
